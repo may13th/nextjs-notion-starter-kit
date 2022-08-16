86dad2c681b227f21f499419a36dcbc84e040e9a
River <52377340+Shydlock@users.noreply.github.com>
2022-08-16 18:09:15 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -19,6 +19,7 @@ export default siteConfig({
   // social usernames (optional)
 //   twitter: 'transitive_bs',
   github: 'Shydlock',
+  zhihu: 'bing-yu-18-12',
 //   linkedin: 'fisch2',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
