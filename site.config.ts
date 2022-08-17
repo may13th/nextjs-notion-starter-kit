2b56db85da20a1b63e758a04bb35512c27e54bdd
River <52377340+Shydlock@users.noreply.github.com>
2022-08-17 13:51:21 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,8 +9,8 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'River',
-  domain: 'liu.he.cn',
+  name: 'Next.js Notion Starter Kit',
+  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
   author: 'River',
 
   // open graph metadata (optional)
