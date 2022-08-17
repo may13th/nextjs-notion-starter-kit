706f91c633efe447effa5dea32b512c7aee34243
River <52377340+Shydlock@users.noreply.github.com>
2022-08-17 12:43:43 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export default siteConfig({
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: 'fdd34d2e690b423e82c734f6dd758380' ,
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
