79be1ea53e0dbdbe35090f1c34d3d13158962b2e
may13th <111521848+may13th@users.noreply.github.com>
2022-08-19 10:18:34 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '36eeec2061e04fa39bab4c6222927a2e',
+  rootNotionPageId: '6fb673db682746cfa1ff68ca9b259186',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'River',
+  name: 'May13th',
   domain: 'osier-river',
-  author: 'River',
+  author: 'LiuHe&GaoJialu',
 
   // open graph metadata (optional)
-  description: 'River Blog',
+  description: 'May13th',
 
   // social usernames (optional)
 //   twitter: 'transitive_bs',
@@ -55,15 +55,15 @@ export default siteConfig({
   navigationLinks: [
     {
       title: 'About',
-      pageId: 'd7a02260bb204b8eb8f7231afbbecef6'
+      pageId: '1f17fb2b65cf4156966c7e402a4391b4'
     },
     {
       title: 'Origin',
-      pageId: '2b1608d9b9784a62a1e3c4427df14c9c'
+      pageId: 'b164749fbc1b421bb8d6b0c3f6f583e1'
     },
     {
       title: 'Contact',
-      pageId: 'a52a8b30f2f34eff877cf92edcc4c4ec'
+      pageId: '7467dc135ab340daacf9fcef41b962a0'
     }
   ]
 })
